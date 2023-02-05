3c115a30c472e6398c86d8a98e2329c9576fc8e3
AkhileshMishra31 <89763916+AkhileshMishra31@users.noreply.github.com>
2023-02-05 22:50:05 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,14 @@
-import React from 'react';
-const Todo = () => {
-    return (
-        <></>
-    )
-}
+import React from "react";
+import "../styles/App.css";
 
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="logo">Todo List</div>
+      <button className="theme">Theme</button>
+    </div>
+  );
+};
 
-export { Todo }
+export default Navbar;
 
